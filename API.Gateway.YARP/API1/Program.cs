8331f01f8762cc4a1cba5db22e085a9789ae7f8c
Ömer temel <42616536+Puyz@users.noreply.github.com>
2024-12-10 22:19:15 +0300

    yarp - auth

--- API.Gateway.YARP/API1/Program.cs
+++ API.Gateway.YARP/API1/Program.cs
@@ -1,10 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Token:Issuer"],
+            ValidAudience = builder.Configuration["Token:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"]!)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Authenticated", policy => policy.RequireAuthenticatedUser());
+});
 
 var app = builder.Build();
 
-app.MapGet("/api1", () => "API 1");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/api1", () => "API 1").RequireAuthorization("Authenticated");
 
 
 app.Run();
